6f4ee71900526e4cd20fe68f348a677d6fd3e39b
Nobubu <kakakatatatakukukurarara@gmail.com>
2026-06-05 01:25:08 +0000

    Updtae app

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head><link rel="manifest" href="/manifest.json" />
-        <meta name="google-site-verification" content="a9d1ACZSw9i3cW483QCIuHXaIu6_sxAjQDCvpKJVFWQ" /></head>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="google-site-verification" content="a9d1ACZSw9i3cW483QCIuHXaIu6_sxAjQDCvpKJVFWQ" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
